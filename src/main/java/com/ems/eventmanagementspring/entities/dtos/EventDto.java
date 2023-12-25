ae9655da6058d9c4ee54dd8c3e707b5bcc765157
kanakx <kacperkanak@icloud.com>
2023-12-25 11:07:39 +0100

    Add EventDto

---
+++ src/main/java/com/ems/eventmanagementspring/entities/dtos/EventDto.java
@@ -0,0 +1,22 @@
+package com.ems.eventmanagementspring.entities.dtos;
+
+import com.ems.eventmanagementspring.entities.models.EventType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class EventDto {
+
+    private Long idEvent;
+    private String name;
+    private EventType type;
+    private LocalDateTime startTimestamp;
+    private LocalDateTime endTimestamp;
+    private String locationName;
+    private String description;
+    private boolean isPublic;
+
+}
