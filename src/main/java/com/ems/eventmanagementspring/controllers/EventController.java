ee276adf0582f529b718ad7a87d25d4fcc29e091
kanakx <kacperkanak@icloud.com>
2023-12-25 15:37:07 +0100

    Add web configuration

--- src/main/java/com/ems/eventmanagementspring/controllers/EventController.java
+++ src/main/java/com/ems/eventmanagementspring/controllers/EventController.java
@@ -3,10 +3,7 @@ package com.ems.eventmanagementspring.controllers;
 import com.ems.eventmanagementspring.entities.dtos.EventDto;
 import com.ems.eventmanagementspring.services.interfaces.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
