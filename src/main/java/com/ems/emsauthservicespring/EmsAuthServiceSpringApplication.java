79657fa6240872434a55859050daca131cb3d5ca
kanakx <kacperkanak@icloud.com>
2024-01-01 22:45:42 +0100

    Initial commit

---
+++ src/main/java/com/ems/emsauthservicespring/EmsAuthServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ems.emsauthservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsAuthServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsAuthServiceSpringApplication.class, args);
+    }
+
+}
