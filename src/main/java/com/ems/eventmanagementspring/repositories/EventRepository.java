02144b49214ee7bcc4367cb471aebbdf82f65347
kanakx <kacperkanak@icloud.com>
2024-01-02 02:14:30 +0100

    Fix bugs

--- src/main/java/com/ems/eventmanagementspring/repositories/EventRepository.java
+++ src/main/java/com/ems/eventmanagementspring/repositories/EventRepository.java
@@ -4,7 +4,6 @@ import com.ems.eventmanagementspring.entities.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
