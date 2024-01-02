58386a46a0977063c0d61fb68fb8fedd87cc6859
kanakx <kacperkanak@icloud.com>
2024-01-02 02:48:34 +0100

    Initial commit

---
+++ src/main/java/com/ems/emseurekaspring/EmsEurekaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ems.emseurekaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsEurekaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsEurekaSpringApplication.class, args);
+    }
+
+}
